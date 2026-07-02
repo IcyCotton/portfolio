c4b1c8e27bfe5b0cb3ca8410a29d4978970428e0
Adrian Justine P. Rodin <ajjustine4@gmail.com>
2026-07-02 08:45:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark');
 });
-
-const yearEl = document.getElementById('year');
-if (yearEl) {
-  yearEl.textContent = new Date().getFullYear();
-}
